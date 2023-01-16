088d5e44b2c8974905de3c6c5bcf01d4a82a19d2
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2023-01-16 15:49:49 +0100

    added

--- IPay/ListPeople.Designer.cs
+++ IPay/ListPeople.Designer.cs
@@ -28,16 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< HEAD
             this.HeadWaiterList = new System.Windows.Forms.RichTextBox();
             this.WaiterList = new System.Windows.Forms.RichTextBox();
             this.CustomerList = new System.Windows.Forms.RichTextBox();
             this.ChefList = new System.Windows.Forms.RichTextBox();
+=======
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+>>>>>>> a4aaa0db1cee0ac15d472d3cb54559fe42b3a9b1
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
+<<<<<<< HEAD
             // HeadWaiterList
             // 
             this.HeadWaiterList.Location = new System.Drawing.Point(12, 40);
@@ -73,6 +81,39 @@
             this.ChefList.Size = new System.Drawing.Size(151, 398);
             this.ChefList.TabIndex = 3;
             this.ChefList.Text = "";
+=======
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(151, 398);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(169, 40);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(151, 398);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(325, 40);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(151, 398);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(482, 40);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(151, 398);
+            this.richTextBox4.TabIndex = 3;
+            this.richTextBox4.Text = "";
+>>>>>>> a4aaa0db1cee0ac15d472d3cb54559fe42b3a9b1
             // 
             // label1
             // 
@@ -120,10 +161,17 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+<<<<<<< HEAD
             this.Controls.Add(this.ChefList);
             this.Controls.Add(this.CustomerList);
             this.Controls.Add(this.WaiterList);
             this.Controls.Add(this.HeadWaiterList);
+=======
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+>>>>>>> a4aaa0db1cee0ac15d472d3cb54559fe42b3a9b1
             this.Name = "ListPeople";
             this.Text = "ListPeople";
             this.Load += new System.EventHandler(this.ListPeople_Load);
@@ -134,10 +182,17 @@
 
         #endregion
 
+<<<<<<< HEAD
         private System.Windows.Forms.RichTextBox HeadWaiterList;
         private System.Windows.Forms.RichTextBox WaiterList;
         private System.Windows.Forms.RichTextBox CustomerList;
         private System.Windows.Forms.RichTextBox ChefList;
+=======
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+>>>>>>> a4aaa0db1cee0ac15d472d3cb54559fe42b3a9b1
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
